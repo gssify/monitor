d4c535b8970c68d3a0c537dbf4fcad99ace7e5e6
宫克 <hzgongke@corp.netease.com>
2021-03-05 07:51:35 +0800

    first commit

---
+++ api.js
@@ -0,0 +1,94 @@
+const axios = require('axios')
+const querystring = require('querystring')
+const { HEADERS, DEPARTMENT_CODE } = require('./const')
+
+function request (url, ref, data = {}) {
+  return new Promise((resolve, reject) => {
+    axios({
+      url,
+      method: 'post',
+      params: { _: Date.now() },
+      data: querystring.stringify(data),
+      headers: {
+        ...HEADERS,
+        Referer: ref
+      }
+    }).then(r => resolve(r.data))
+      .catch(reject)
+  })
+}
+
+async function getUserInfo (openId) {
+  try {
+    const result = await request(
+      'http://pay.ayfy.com/ayfy/rest/chaxunJiuzhenrenXinxi',
+      'http://pay.ayfy.com/ayfy/webpage/wxpg/hao-yuan-fen-shi.html',
+      { openId,
+        context: 1
+      }
+    )
+    return result.msg[0]
+  } catch (error) {
+    return null
+  }
+}
+
+async function getSchedule (date) {
+  try {
+    const result = await request(
+      'http://pay.ayfy.com/ayfy/rest/QueryAdmSchedule',
+      'http://pay.ayfy.com/ayfy/webpage/wxpg/hao-yuan-xin-xi.html',
+      {
+        yydm: 'CXSPBQHTSYQ',
+        DepartmentCode: DEPARTMENT_CODE,
+        StartDate: date,
+        EndDate: date,
+        DoctCode: '',
+        context: 1
+      }
+    )
+    return result.obj.Schedules.Schedule
+  } catch (error) {
+    return null
+  }
+}
+
+async function bookSchedule (user, schedule) {
+  try {
+    const result = await request(
+      'http://pay.ayfy.com/ayfy/rest/BookService',
+      'http://pay.ayfy.com/ayfy/webpage/wxpg/que-ren-gua-hao.html',
+      {
+        yydm: 'CXSPBQHTSYQ',
+        gender: user.sex,
+        openId: user.openIdWeixin,
+        orderType: '05',
+        OrderAmount: 0,
+        PatientName: user.hzxm,
+        PatientCard: user.cardno,
+        PatientID: user.patid,
+        ScheduleItemCode: schedule.ScheduleItemCode,
+        BeginTime: schedule.StartTime,
+        EndTime: schedule.EndTime,
+        DepartmentCode: DEPARTMENT_CODE,
+        DepartmentName: schedule.DepartmentName,
+        DoctorCode: schedule.DoctorCode,
+        DoctorName: schedule.DoctorName,
+        AdmitAddress: schedule.AdmitAddress,
+        SessionName: schedule.SessionName,
+        date: schedule.ServiceDate,
+        AvailableNumStr: schedule.AvailableNumStr,
+        context: 1
+      }
+    )
+    return { success: !!result.obj.OrderContent, msg: result.obj.OrderContent }
+  } catch (error) {
+    return { success: false, msg: '' }
+  }
+}
+
+module.exports = {
+  getSchedule,
+  getUserInfo,
+  bookSchedule
+}
---
+++ const.js
@@ -0,0 +1,13 @@
+exports.HEADERS = {
+  'X-Requested-With': 'XMLHttpRequest',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1301.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.5 WindowsWechat'
+}
+
+exports.DEPARTMENT_CODE = 1349 // 绩溪路排班
+
+exports.WORK_TYPE = '咽拭子采集'
+
+exports.SESSION = {
+  AM: '上午',
+  PM: '下午'
+}
